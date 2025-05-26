0b6573484792f7e25f89c2fb0e0d7b3d1c37bcef
Kelvin <kelvindelima710@gmail.com>
2025-05-26 20:03:41 -0300

    feat: implement CounterController at app view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx_practice/counter_controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,13 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final _counterController = CounterController();
 
   @override
   Widget build(BuildContext context) {
@@ -48,16 +44,18 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Observer(
+              builder:
+                  (_) => Text(
+                    'Você apertou o botão ${_counterController.count} vezes',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _counterController.increment,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
